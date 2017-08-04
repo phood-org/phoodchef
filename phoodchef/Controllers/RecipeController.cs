d5ffaefe3e52038220805b2d4b9925ad2657adb5
Pearse Hutson <pearse.2008@gmail.com>
2017-08-04 15:20:39 -0500

    Initial commit

---
+++ phoodchef/Controllers/RecipeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using phoodchef.Models.DTOs;
+
+namespace phoodchef.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        // For reference: public async Task<IHttpActionResult> GetAllRecipes()
+        [HttpGet]
+        [Route("api/recipes")]
+        public IHttpActionResult GetAllRecipes()
+        {
+            List<RecipeDto> recipes = new List<RecipeDto>
+            {
+                new RecipeDto
+                {
+                    Id = 1,
+                    Name = "Pepperocini Beef",
+                    CookTime = TimeSpan.FromHours(8),
+                    CookUnit = "Hours",
+                    Instructions = "Buy Beef and Pepperocinis. Put in crock pot. Eat",
+                    ServeMax = 4.0,
+                    ServeMin = 2.0,
+                    Yield = 4.0
+                },
+                new RecipeDto
+                {
+                    Id = 1,
+                    Name = "Spagetti",
+                    CookTime = TimeSpan.FromMinutes(30),
+                    CookUnit = "Minutes",
+                    Instructions = "Noodles and Ragu. Done.",
+                    ServeMax = 10.0,
+                    ServeMin = 2.0,
+                    Yield = 4.0
+                }
+            };
+
+            return Ok(recipes);
+        }
+    }
+}
---
+++ phoodchef/Models/DTOs/RecipeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace phoodchef.Models.DTOs
+{
+    public class RecipeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan CookTime { get; set; }
+        public string CookUnit { get; set; }
+        public string Instructions { get; set; }
+        public double Yield { get; set; }
+        public double ServeMin { get; set; }
+        public double ServeMax { get; set; }
+    }
+}
